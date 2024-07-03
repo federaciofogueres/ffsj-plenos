81248ed3a0e4d70ca1aeebc8327583e1a96db701
Fran García <fgarciadevelop@gmail.com>
2024-07-03 23:25:55 +0200

    feat: change url

--- src/app/components/votaciones/votaciones.component.ts
+++ src/app/components/votaciones/votaciones.component.ts
@@ -42,9 +42,12 @@ export class VotacionesComponent {
   goToVotacion(votacion: Votacion) {
     console.log('Ir a votación', votacion);
     // this.cookieService.set('href', 'https://plenos.hogueras.es');
-    this.cookieService.set('href', 'http://localhost:4201');
+    this.cookieService.set('href', 'https://polite-pond-09776c203.5.azurestaticapps.net/');
+    
+    // this.cookieService.set('href', 'http://localhost:4201');
     // window.location.href = 'https://consultas.hogueras.es';
-    window.location.href = 'http://localhost:4200';
+    window.location.href = 'https://gray-forest-0c4b60603.5.azurestaticapps.net/';
+    // window.location.href = 'http://localhost:4200';
   }
 
 }
