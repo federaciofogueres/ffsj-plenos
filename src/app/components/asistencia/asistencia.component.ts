556c1340959b646d3f80cda47fcae40e2bc5e8d2
Fran García <fgarciadevelop@gmail.com>
2024-07-06 19:10:33 +0200

    feat: asistencia

--- src/app/components/asistencia/asistencia.component.ts
+++ src/app/components/asistencia/asistencia.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../services/validators.service';
 
 @Component({
   selector: 'app-asistencia',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './asistencia.component.html',
   styleUrl: './asistencia.component.scss'
 })
 export class AsistenciaComponent {
 
+  showDelegacionAsistenciaForm = false;
+  delegacionAsistenciaForm: FormGroup = this.fb.group({
+    dni: ['', [Validators.required, this.validatorsService.dniValidator]],
+  });
+  dniTouched = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) { }
+
+  delegarAsistencia() {
+    console.log('Delegar asistencia a: ', this.delegacionAsistenciaForm.value.dni);
+    
+    this.showDelegacionAsistenciaForm = false;
+  }
 }
