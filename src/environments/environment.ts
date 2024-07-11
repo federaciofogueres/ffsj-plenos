2458756e943236e190e6cd93f3a072c6e8038f2e
Fran García <fgarciadevelop@gmail.com>
2024-07-11 22:40:54 +0200

    feat: add environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    API_BASE_PATH: 'https://ffsj-plenos-api.azurewebsites.net/FFSJ/APP-PLENOS/1.0.0',
+    FILES_BASE_PATH: 'https://files.censo.hogueras.es/uploads'
+};
