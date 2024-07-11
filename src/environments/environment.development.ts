e845cf6ea132eb9e1bed5eac4bef4017cf350622
Fran García <fgarciadevelop@gmail.com>
2024-07-12 00:48:52 +0200

    feat: environment change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_BASE_PATH: 'http://localhost:8080/FFSJ/APP-PLENOS/1.0.0',
-    // API_BASE_PATH: 'https://ffsj-plenos-api.azurewebsites.net/FFSJ/APP-PLENOS/1.0.0',
+    // API_BASE_PATH: 'http://localhost:8080/FFSJ/APP-PLENOS/1.0.0',
+    API_BASE_PATH: 'https://ffsj-plenos-api.azurewebsites.net/FFSJ/APP-PLENOS/1.0.0',
     FILES_BASE_PATH: 'https://files.censo.hogueras.es/uploads'
 };
