ec1fc0d1d30918997999e85492c98ee49eec8f87
Fran García <fgarciadevelop@gmail.com>
2024-07-15 21:24:28 +0200

    feat: buttons back fix

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { FfsjSpinnerComponent } from 'ffsj-web-components';
 import { Pleno, PuntoOrdenDelDia, PuntosOrdenDelDiaService } from '../../../api';
@@ -21,6 +21,7 @@ export class FormularioComponent {
 
   loading: boolean = false;
 
+  @Output() back: EventEmitter<void> = new EventEmitter();
   @Input() pleno: Pleno | null = null;
   puntosOrdenDelDia: PuntoOrdenDelDia[] = [];
 
