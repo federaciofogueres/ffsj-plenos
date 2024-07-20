ecee1cd6470b135355d0d8f1fa1a95c44ff1e2e6
Fran García <fgarciadevelop@gmail.com>
2024-07-20 10:44:43 +0200

    feat: fail on suscriptions

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'ffsj-web-components';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit, OnDestroy {
   userLogged: boolean = false;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     protected router: Router,
@@ -20,17 +22,22 @@ export class FooterComponent {
 
   ngOnInit() {
     this.userLogged = this.authService.isLoggedIn();
-    this.authService.loginStatusObservable.subscribe({
+    this.authService.loginStatusObservable.pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe({
       next: (loggedIn: boolean) => {
         console.log('User logged in:', loggedIn);
-        
         this.userLogged = loggedIn;
       }
-    })
+    });
   }
 
   redirectTo(url: string) {
     this.router.navigate([url]);
   }
-  
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'ffsj-web-components';
+import { Subject, takeUntil } from 'rxjs';
 // import { AuthService } from '../../services/auth.service';
-
-
 
 @Component({
   selector: 'app-header',
@@ -19,6 +18,7 @@ import { AuthService } from 'ffsj-web-components';
 export class HeaderComponent {
   userLogged: boolean = false;
   userAdmin: boolean = false;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     protected router: Router,
@@ -27,13 +27,14 @@ export class HeaderComponent {
 
   ngOnInit() {
     this.userLogged = this.authService.isLoggedIn();
-    this.authService.loginStatusObservable.subscribe({
+    this.authService.loginStatusObservable.pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe({
       next: (loggedIn: boolean) => {
         console.log('User logged in:', loggedIn);
-        
         this.userLogged = loggedIn;
       }
-    })
+    });
   }
 
   logout() {
@@ -41,4 +42,8 @@ export class HeaderComponent {
     this.router.navigateByUrl('');
   }
 
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
