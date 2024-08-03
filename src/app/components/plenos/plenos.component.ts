20ee30b01c1aa618988b4d37af33b2a54b3fde4b
Fran García <fgarciadevelop@gmail.com>
2024-08-03 21:04:18 +0200

    feat: change styles

--- src/app/components/plenos/plenos.component.ts
+++ src/app/components/plenos/plenos.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { FfsjSpinnerComponent } from 'ffsj-web-components';
@@ -8,17 +9,20 @@ import { AsistenciaService, Pleno, PlenoService } from '../../../api';
   selector: 'app-plenos',
   standalone: true,
   imports: [
-    FfsjSpinnerComponent
+    FfsjSpinnerComponent,
+    CommonModule
   ],
   templateUrl: './plenos.component.html',
   styleUrl: './plenos.component.scss'
 })
 export class PlenosComponent {
 
-  plenos: Pleno[] = [];
   loading: boolean = false;
   mensajeError: string = '';
   idAsociado: number = -1;
+  inactivePlenos: Pleno[] = [];
+  activePlenos: Pleno[] = [];
+  showPlenos: string = 'active';
 
   @Input() seleccionaPleno: boolean = false;
   @Output() plenoSeleccionado = new EventEmitter<number>();
@@ -35,6 +39,22 @@ export class PlenosComponent {
     this.loadPlenos();
   }
 
+  formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Los meses en JavaScript son 0-indexados
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${day}/${month}/${year} - ${hours}:${minutes}h`;
+  }
+
+  isActivePleno(plenoDate: Date): boolean {
+    const now = new Date();
+    const twoHoursBefore = new Date(now.getTime() - 2 * 60 * 60 * 1000);
+    const fourHoursAfter = new Date(now.getTime() + 4 * 60 * 60 * 1000);
+    return plenoDate >= twoHoursBefore && plenoDate <= fourHoursAfter;
+  }
+
   loadPlenos() {
     this.idAsociado = parseInt(this.cookieService.get('idUsuario'));
     this.asistenciaService.asistenciaIdGet(this.idAsociado).subscribe({
@@ -44,7 +64,13 @@ export class PlenosComponent {
           this.plenosService.plenoIdGet(asistencia.idPleno).toPromise().then((response: any) => {
             console.log('Pleno:', response);
             if (response.status.status === 200) {
-              this.plenos.push(response.plenos[0]);
+              let pleno = response.plenos[0]
+              pleno.fecha = this.formatDate(new Date(pleno.fecha));
+              if (this.isActivePleno(pleno.fecha)) {
+                this.activePlenos.push(pleno);
+              } else {
+                this.inactivePlenos.push(pleno);
+              }
             }
           }).catch((error: any) => {
             console.log('Error:', error);
